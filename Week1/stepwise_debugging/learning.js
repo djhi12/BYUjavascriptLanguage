e95fe9ef905b29d5cdedc20095678d21c0157618
Daniel John Idorot <ido20002@byui.edu>
2023-02-22 06:22:28 +0800

    Initial commit

--- Week1/stepwise_debugging/learning.js
+++ Week1/stepwise_debugging/learning.js
@@ -1,5 +1,5 @@
 
-let currentDateAndTime = new Date()
+let currentDateAndTime = new Date();
 
 console.log("It is now " + currentDateAndTime);
 
@@ -12,7 +12,7 @@ function total(...theNumbers){
 	let sum = 0;
 
 	for(let aNumber in theNumbers){
-		sum += aNumber*1//Want to know why aNumber is multiplied by 1? Remove it and find out. :)
+		sum += aNumber*1; //Want to know why aNumber is multiplied by 1? Remove it and find out. :)
 	}
 
 	return sum;
